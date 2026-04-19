8850d75afa2413c5246b4280aa692a169ed29dbc
Saj1d-prompt <sajidulislam2225@gmail.com>
2026-04-19 18:06:12 +0600

    Footer Section created

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import Heading from './Component/Common/Heading';
 import HeroSection from './Component/Common/HeroSection';
 import Feature from './Component/Common/Feature';
 import About from './Component/Common/About';
+import Footer from './Component/Common/Footer';
 function App() {
   return (
     <>
@@ -11,6 +12,7 @@ function App() {
     <HeroSection></HeroSection>
     <Feature></Feature>
     <About/>
+    <Footer/>
     </>
   )
 }
