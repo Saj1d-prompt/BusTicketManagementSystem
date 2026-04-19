dba9611f1dc93ca9b622a973e2d2479a5b11ea82
Saj1d-prompt <sajidulislam2225@gmail.com>
2026-04-19 18:28:00 +0600

    Routing Setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,17 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
+import { Route, Routes, BrowserRouter } from 'react-router-dom'
 import LandingPage from './Component/Common/LandingPage';
 function App() {
   return (
     <>
-      <LandingPage/>
+      {/* <LandingPage/> */}
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+        </Routes>
+      </BrowserRouter>
+
     </>
   )
 }
