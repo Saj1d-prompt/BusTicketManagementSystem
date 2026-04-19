fe6f92dee3f3c57b263fc0d7a3d5c5d39abd2f0d
Saj1d-prompt <sajidulislam2225@gmail.com>
2026-04-19 18:20:30 +0600

    Landing Page Component Completed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,10 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
-import Heading from './Component/Common/Heading';
-import HeroSection from './Component/Common/HeroSection';
-import Feature from './Component/Common/Feature';
-import About from './Component/Common/About';
-import Footer from './Component/Common/Footer';
+import LandingPage from './Component/Common/LandingPage';
 function App() {
   return (
     <>
-    <Heading></Heading>
-    <HeroSection></HeroSection>
-    <Feature></Feature>
-    <About/>
-    <Footer/>
+      <LandingPage/>
     </>
   )
 }
---
+++ frontend/src/Component/Common/LandingPage.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Heading from './Heading'
+import HeroSection from './HeroSection'
+import Feature from './Feature'
+import About from './About'
+import Footer from './Footer'
+
+const LandingPage = () => {
+  return (
+    <div>
+      <Heading/>
+      <HeroSection/>
+      <Feature/>
+      <About/>
+      <Footer/>
+    </div>
+  )
+}
+
+export default LandingPage
