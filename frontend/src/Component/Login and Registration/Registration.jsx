89513013ca6119f26ce340238a14d8303a130b78
Saj1d-prompt <sajidulislam2225@gmail.com>
2026-04-20 15:35:06 +0600

    Added Role based Create Account form loading

--- frontend/src/Component/Login and Registration/Registration.jsx
+++ frontend/src/Component/Login and Registration/Registration.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import { useState } from 'react';
+import PassengerReg from './PassengerReg';
+import BusCompanyReg from './BusCompanyReg';
+import SelectRegistration from './SelectRegistration';
 
 const Registration = () => {
+  const [role, setRole] = useState(null);
+  const renderContent = () => {
+    if (role === 'passenger') {
+      return <PassengerReg onBack={() => setRole(null)} />;
+    }
+    if (role === 'company') {
+      return <BusCompanyReg onBack={() => setRole(null)} />;
+    }
+    return <SelectRegistration onSelectRole={setRole} />;
+  };
   return (
     <div>
-      
+      {renderContent()}
     </div>
   )
 }
