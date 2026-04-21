27f48dcac01c2555d8f6872bcc0accd6218c0a3e
Saj1d-prompt <sajidulislam2225@gmail.com>
2026-04-21 12:13:02 +0600

    Fixed API Route

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -3,9 +3,11 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\supportController;
+use App\Http\Controllers\RegistrationController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
 Route::post('/storeSupport', [supportController::class, 'storeSupportQuestion']);
+Route::post('/registerPassenger', [RegistrationController::class, 'registerPassenger']);
