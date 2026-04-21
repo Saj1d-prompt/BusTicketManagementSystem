527aebadafa49e3db888b3d53490a06451d3dec1
Saj1d-prompt <sajidulislam2225@gmail.com>
2026-04-21 11:28:18 +0600

    Company model class attributes added

--- backend/app/Models/Company.php
+++ backend/app/Models/Company.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Company extends Model
 {
-    //
+    protected $fillable = [
+        'company_name',
+        'license_number',
+        'document_path',
+        'status',
+    ];
+
+    public function users()
+    {
+        return $this->hasMany(User::class);
+    }
 }
