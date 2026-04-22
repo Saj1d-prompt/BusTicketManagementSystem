5a22be7655c2e8895efb00713b26b8ffd9814d73
Saj1d-prompt <sajidulislam2225@gmail.com>
2026-04-22 17:57:10 +0600

    Validating Data and passing error

--- backend/app/Http/Controllers/UserController.php
+++ backend/app/Http/Controllers/UserController.php
@@ -3,10 +3,20 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class UserController extends Controller
 {
     public function login(Request $request){
-        //code for login
+        $validate = Validator::make($request->all(), [
+            'email' => 'required|string|email|max:255',
+            'password' => 'required|string|min:5',
+        ]);
+        if($validate->fails()){
+            return response()->json([
+                'status'=> 400,
+                'errors'=>$validate->errors()
+            ], 400);
+        }
     }
 }
