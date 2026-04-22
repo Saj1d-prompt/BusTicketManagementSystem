eaf2ac810bd28c2fef44412b2cbdb3e995276065
Saj1d-prompt <sajidulislam2225@gmail.com>
2026-04-22 17:59:15 +0600

    Checked Password with user password

--- backend/app/Http/Controllers/UserController.php
+++ backend/app/Http/Controllers/UserController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+use Exception;
 
 class UserController extends Controller
 {
@@ -18,5 +21,13 @@ class UserController extends Controller
                 'errors'=>$validate->errors()
             ], 400);
         }
+        $user = User::where('email', $request->email)->first();
+        if(!$user || !Hash::check($request->password, $user->password)){
+            return response()->json([
+                'status'=> 400,
+                'message'=>'Invalid email or password'
+            ], 400);
+        }
+        
     }
 }
