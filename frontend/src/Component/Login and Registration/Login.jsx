7465162dfb1eb2a37203f063e14e6c11bb58f4dc
Saj1d-prompt <sajidulislam2225@gmail.com>
2026-04-22 17:38:23 +0600

    Modified Login Page

--- frontend/src/Component/Login and Registration/Login.jsx
+++ frontend/src/Component/Login and Registration/Login.jsx
@@ -10,15 +10,15 @@ const Login = () => {
       <h2 className="text-center mb-4 m-4">Login Here</h2>
       <Form className="mx-auto" style={{ maxWidth: '500px' }}>
         <Form.Group className="mb-3 text-start" controlId="formBasicEmail">
-          <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter your email" />
+          <Form.Label className="fw-semibold">Email address</Form.Label>
+          <Form.Control type="email" placeholder="Enter your email" className="py-2" />
         </Form.Group>
         <Form.Group className="mb-3 text-start" controlId="formBasicPassword">
-          <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Enter your password" />
+          <Form.Label className="fw-semibold">Password</Form.Label>
+          <Form.Control type="password" placeholder="Enter your password" className="py-2" />
         </Form.Group>
-        <Button variant="danger" type="submit" className="w-100">
-          Register
+        <Button variant="danger" type="submit" className="w-100 py-2 fw-bold" style={{ borderRadius: '10px' }}>
+          Login
         </Button>
       </Form>
     </div>
