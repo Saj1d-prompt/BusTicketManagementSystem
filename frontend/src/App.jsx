ccb90c037087e086b556479aab21a83cb432633a
Saj1d-prompt <sajidulislam2225@gmail.com>
2026-04-22 17:34:22 +0600

    Designed Login Form

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import { Route, Routes, BrowserRouter } from 'react-router-dom'
 import LandingPage from './Component/Common/LandingPage';
 import SelectRegistration from './Component/Login and Registration/SelectRegistration';
 import Registration from './Component/Login and Registration/Registration';
+import Login from './Component/Login and Registration/Login';
 function App() {
   return (
     <>
@@ -13,6 +14,7 @@ function App() {
         <Routes>
           <Route path="/" element={<LandingPage />} />
           <Route path="/register" element={<Registration />} />
+          <Route path="/login" element={<Login />} />
         </Routes>
       </BrowserRouter>
 
--- frontend/src/Component/Login and Registration/Login.jsx
+++ frontend/src/Component/Login and Registration/Login.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import Heading from '../Common/Heading';
 
 const Login = () => {
   return (
     <div>
-      
+      <Heading />
+      <h2 className="text-center mb-4 m-4">Login Here</h2>
+      <Form className="mx-auto" style={{ maxWidth: '500px' }}>
+        <Form.Group className="mb-3 text-start" controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type="email" placeholder="Enter your email" />
+        </Form.Group>
+        <Form.Group className="mb-3 text-start" controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" placeholder="Enter your password" />
+        </Form.Group>
+        <Button variant="danger" type="submit" className="w-100">
+          Register
+        </Button>
+      </Form>
     </div>
   )
 }
