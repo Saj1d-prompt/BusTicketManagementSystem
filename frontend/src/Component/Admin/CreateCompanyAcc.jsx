c2deb589ab8cc8fa4e0e11d6a23f2807071b6a3c
Saj1d-prompt <sajidulislam2225@gmail.com>
2026-04-25 14:51:23 +0600

    Made Table Structure for Company requests

--- frontend/src/Component/Admin/CreateCompanyAcc.jsx
+++ frontend/src/Component/Admin/CreateCompanyAcc.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
-
+import { Table, Button, Container, Card, Spinner, Badge } from 'react-bootstrap';
 const CreateCompanyAcc = () => {
   return (
     <div>
-      
+      <Container className="py-5">
+        <Card>
+          <Card.Body>
+            <h3>Pending Company Approvals</h3>
+            <Table>
+              <thead>
+                
+              </thead>
+              <tbody>
+
+              </tbody>
+            </Table>
+          </Card.Body>
+        </Card>
+      </Container>
     </div>
   )
 }
