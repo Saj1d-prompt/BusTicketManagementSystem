b3921383915c754b38807991697e5e4008f9f0fe
Saj1d-prompt <sajidulislam2225@gmail.com>
2026-04-30 13:52:14 +0600

    Starting Company Dashboard loding based on Status

---
+++ frontend/src/Component/Business Owner/CompanyDashboardNotifier.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CompanyDashboardNotifier = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default CompanyDashboardNotifier
--- frontend/src/Component/Business Owner/OwnerDashboard.jsx
+++ frontend/src/Component/Business Owner/OwnerDashboard.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import AddOperator from './AddOperator'
 
 const OwnerDashboard = () => {
   return (
     <div>
-      
+      <AddOperator />
     </div>
   )
 }
