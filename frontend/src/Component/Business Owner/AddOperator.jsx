44cce49f5704acc2a51ab752f328e9c26558007c
Saj1d-prompt <sajidulislam2225@gmail.com>
2026-04-30 15:28:31 +0600

    Add operator Card designed

--- frontend/src/Component/Business Owner/AddOperator.jsx
+++ frontend/src/Component/Business Owner/AddOperator.jsx
@@ -1,11 +1,26 @@
 import React from 'react'
+import { Card } from 'react-bootstrap';
 
 const AddOperator = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+    return (
+        <div className="d-flex justify-content-center align-items-center"
+            style={{ minHeight: "100vh", backgroundColor: "#f5f7fa" }}>
+            <Card className="shadow-sm p-4" style={{
+                width: "480px",
+                borderRadius: "14px",
+                background: "#ffffff",
+                border: "1px solid #e3e6ea",
+            }}>
+                <h4 className="text-center mb-3 fw-bold" style={{ color: "#333" }}>
+                    Add Ticket Operator
+                </h4>
+
+            </Card>
+
+
+
+        </div>
+    )
 }
 
 export default AddOperator
