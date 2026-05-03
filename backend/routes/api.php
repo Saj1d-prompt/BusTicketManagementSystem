2ae11b36bd968261b9f51c5399ff9caeb5801394
Saj1d-prompt <sajidulislam2225@gmail.com>
2026-05-03 07:34:05 +0600

    Added OL List route

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -25,4 +25,5 @@ Route::middleware(['auth:sanctum', 'role:admin'])->group(function () {
 Route::middleware(['auth:sanctum', 'role:company'])->group(function () {
     Route::get('/companyStatus', [CompanyOwnerController::class, 'getCompanyStatus']);
     Route::post('/registerOperator', [RegistrationController::class, 'registerOperator']);
+    Route::get('/operatorList', [CompanyOwnerController::class, 'getOperatorList']);
 });
