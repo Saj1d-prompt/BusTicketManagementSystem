e3fafcbd2661b948d364a85b2ba1cf618b9818c4
Saj1d-prompt <sajidulislam2225@gmail.com>
2026-05-12 11:50:47 +0600

    Imported usestates and useforms

--- frontend/src/Component/Business Owner/AddRoute.jsx
+++ frontend/src/Component/Business Owner/AddRoute.jsx
@@ -1,13 +1,21 @@
 import React from 'react'
+import { useContext } from 'react';
+import { AuthContext } from '../Context/AuthContext';
+import { useState } from 'react';
+import { useForm } from 'react-hook-form';
 
 
 const BANGLADESH_CITIES = ['Dhaka', 'Faridpur', 'Gazipur', 'Gopalganj', 'Jamalpur', 'Kishoreganj', 'Madaripur', 'Manikganj', 'Munshiganj', 'Mymensingh', 'Narayanganj', 'Narsingdi', 'Netrokona', 'Rajbari', 'Shariatpur', 'Sherpur', 'Tangail'
   ,'Bogra', 'Joypurhat','Naogaon','Natore','Nawabganj','Pabna','Rajshahi','Sirajgonj','Dinajpur','Gaibandha','Kurigram','Lalmonirhat','Nilphamari','Panchagarh','Rangpur','Thakurgaon', 'Barguna', 'Barisal', 'Bhola', 'Jhalokati',
   'Patuakhali', 'Pirojpur', 'Bandarban', 'Brahmanbaria', 'Chandpur', 'Chittagong', 'Comilla', 'Cox\'s Bazar', 'Feni', 'Khagrachari', 'Lakshmipur', 'Noakhali', 'Rangamati', 'Habiganj', 'Maulvibazar', 'Sunamganj', 'Sylhet',
-  'Bagerhat', 'Chuadanga', 'Jessore', 'Jhenaidah', 'Khulna', 'Kushtia', 'Magura', 'Meherpur', 'Narail','Satkhira']
+  'Bagerhat', 'Chuadanga', 'Jessore', 'Jhenaidah', 'Khulna', 'Kushtia', 'Magura', 'Meherpur', 'Narail','Satkhira'].sort();
 
 
 const AddRoute = () => {
+  const {user} = useContext(AuthContext);
+  const [loading, setLoading] = useState(false);
+  const [message, setMessage] = useState(null);
+  const { register, handleSubmit, reset } = useForm();
   return (
     <div>
       
