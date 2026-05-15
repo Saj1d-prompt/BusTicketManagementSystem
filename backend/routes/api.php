39fd967ef10677406b6533c8a4422cb5f391c7bc
Saj1d-prompt <sajidulislam2225@gmail.com>
2026-05-15 12:25:07 +0600

    Added API Route

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -33,4 +33,5 @@ Route::middleware(['auth:sanctum', 'role:company'])->group(function () {
     Route::put('/updateBusStatus/{id}', [CompanyOwnerController::class, 'updateBusStatus']);
     Route::post('/addBusRoute', [CompanyOwnerController::class, 'addRoute']);
     Route::get('/routeList', [CompanyOwnerController::class, 'getRouteList']);
+    Route::put('/updateRouteStatus/{id}', [CompanyOwnerController::class, 'updateRouteStatus']);
 });
